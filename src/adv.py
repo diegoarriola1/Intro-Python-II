0da99ad543359db4794e7a1120999cac802999c8
Diego Arriola <diego@macbook-pro.lan>
2020-09-08 13:10:47 -0400

    assignment 1 upload

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -18,7 +19,7 @@ to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south or quit with 'q'."""),
 }
 
 
@@ -38,14 +39,55 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player1 = Player(room["outside"])
 
 # Write a loop that:
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
+
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+# TODO Input parser REPL
+while True:
+    # print current location and room description
+    print()
+    print(f"Current Location: {player1.room.name}")
+    print(f"Hint: {player1.room.description}")
+    print()
+    # take in user input
+    move = str(input("Which direction would you like to go?: ")).lower()
+    moves = ["n", "s", "e", "w", "q"]
+    # parse through input and evaluate
+    if move in moves:
+        if move == 'n':
+            if hasattr(player1.room, 'n_to'):
+                player1.room = player1.room.n_to
+            else:
+                print(f"Oops nothing there still at {player1.room.name}")
+
+        elif move == 's':
+            if hasattr(player1.room, 's_to'):
+                player1.room = player1.room.s_to
+            else:
+                print(f"Oops nothing there still at {player1.room.name}")
+        elif move == 'e':
+            if hasattr(player1.room, 'e_to'):
+                player1.room = player1.room.e_to
+            else:
+                print(f"Oops nothing there still at {player1.room.name}")
+        elif move == 'w':
+            if hasattr(player1.room, 'w_to'):
+                player1.room = player1.room.w_to
+            else:
+                print(f"Oops nothing there still at {player1.room.name}")
+        elif move == 'q':
+            print('Thanks for playing!')
+            break
+    else:
+        print("Please enter proper command (n, s, e, w)")
